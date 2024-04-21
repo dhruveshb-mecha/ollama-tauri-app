5bdd65d7d65d6852b3b47d79361cfdd3b1e1b0e0
dhruveshb-mecha <dhruveshb@mechasystems.com>
2024-04-20 22:57:41 -0400

    feat: text to speech and speech to text added

--- app/icons.jsx
+++ app/icons.jsx
@@ -16,20 +16,84 @@ export function UserIcon() {
   );
 }
 
-
 export function AiIcon() {
-    return (
-      <div className="rounded-full bg-gray-100 border p-1">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          strokeWidth="1.5"
-          ariaHidden="true"
-          viewBox="0 0 24 24"
-        >
-          <path d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zm8.446-7.189L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zm-1.365 11.852L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"></path>
-        </svg>
-      </div>
-    );
-  }
+  return (
+    <div className="rounded-full bg-gray-100 border p-1">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        strokeWidth="1.5"
+        aria-hidden="true"
+        viewBox="0 0 24 24"
+      >
+        <path d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zm8.446-7.189L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zm-1.365 11.852L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"></path>
+      </svg>
+    </div>
+  );
+}
+
+export function RecordIcon(props) {
+  const { listening } = props;
+  return (
+    <button
+      onContextMenu={(e) => e.preventDefault()}
+      className={`p-3 rounded-full ${
+        listening === "true" ? "bg-green-400" : "bg-gray-200"
+      } hover:bg-gray-300 focus:outline-none`}
+      {...props}
+      listening={listening.toString()}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z"
+        />
+      </svg>
+    </button>
+  );
+}
+
+export const SpeechButton = (props) => {
+  console.log("Mic Button props ==>", props);
+  return (
+    <button
+      onContextMenu={(e) => e.preventDefault()}
+      className={`pointer-events-none p-3 rounded-full focus:outline-none  focus:pointer-events-auto
+    ${props.listening ? "bg-green-300" : "bg-gray-200"}
+  `}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z"
+        />
+      </svg>
+    </button>
+  );
+};
+
+export const Spinner = () => {
+  return (
+    <button type="button" className="bg-indigo-500 ..." disabled>
+      <svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>
+      Processing...
+    </button>
+  );
+};
--- app/page.js
+++ app/page.js
@@ -1,12 +1,49 @@
 "use client";
+import "regenerator-runtime/runtime";
 import { useState, useEffect, useRef } from "react";
-import Input from "./input";
-import { AiIcon, UserIcon } from "./icons";
-import { fetch, Body } from "@tauri-apps/api/http";
+import Script from "next/script";
+import http from "http";
+
+import { AiIcon, UserIcon, RecordIcon, SpeechButton, Spinner } from "./icons";
+import OpenAI from "openai";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+
+import { useLongPress } from "@uidotdev/usehooks";
 
 export default function Home() {
+  const openai = new OpenAI({
+    apiKey: process.env["OPENAI_API_KEY"],
+    dangerouslyAllowBrowser: true,
+  });
+  //console api key
+  console.log(process.env["OPENAI_API_KEY"]);
+
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
+  const [isSending, setIsSending] = useState(false);
+
+  // Speech recognition
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  // this function is speech from text
+  const handlePlay = (message) => {
+    // Check if responsiveVoice is loaded
+    console.log("Playing");
+    console.log("Message: ", message);
+    if (responsiveVoice) {
+      console.log("ResponsiveVoice loaded");
+      responsiveVoice.speak(message, "UK English Male");
+    } else {
+      console.error("ResponsiveVoice not loaded");
+    }
+  };
 
   const messagesEndRef = useRef(null);
 
@@ -16,60 +53,110 @@ export default function Home() {
 
   useEffect(scrollToBottom, [messages]);
 
-  const handleSend = async () => {
-    const payload = Body.json({
-      model: "mecha-llm",
-      stream: false,
+  const attrs = useLongPress(
+    (event) => {
+      console.log("Pressing");
+      SpeechRecognition.startListening();
+    },
+    {
+      onStart: (event) => {
+        console.log("Press started");
+      },
+      onFinish: (event) => {
+        console.log("Message: ", transcript);
+        setMessage(transcript);
+        SpeechRecognition.stopListening();
+      // Send the transcript as a message only if isSending is false
+        if (!isSending) {
+          handleSend(transcript);
+        }
+      },
+      onCancel: (event) => {
+        console.log("Press cancelled");
+        SpeechRecognition.stopListening(); // Stop listening if the press is cancelled
+      },
+      threshold: 500,
+    }
+  );
+
+  const handleSend = async (message) => {
+    if (isSending) return; // If an API call is already in progress, don't start a new one
+
+    const payload = {
+      model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: message }],
-    });
+    };
 
     if (message !== "") {
-      setMessages((prevMessages) => [...prevMessages, { role: "user", content: message }]); // Update the messages
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { role: "user", content: message },
+      ]); // Update the messages
       try {
-        const response = await fetch("http://localhost:11434/api/chat", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: payload,
-        });
-        const data = await response.data;
+        setIsSending(true); // Set the flag to true to indicate that an API call is in progress
+        const chatCompletion = await openai.chat.completions.create(payload);
+        console.log(chatCompletion);
         setMessages((prevMessages) => [
           ...prevMessages,
-          { role: "ai", content: data.message.content },
+          { role: "ai", content: chatCompletion.choices[0].message.content },
         ]); // Add the new response to the array
         setMessage(""); // Clear the input field
+        handlePlay(chatCompletion.choices[0].message.content); // Convert the AI's response to speech
       } catch (error) {
         console.error("Error:", error);
+      } finally {
+        setIsSending(false); // Reset the flag after the API call is finished
       }
     }
   };
 
   return (
-    <main className="flex flex-col items-center justify-between w-full h-screen bg-white">
-      <div className="mb-auto overflow-auto w-full">
-        <div className="m-2">
-          {messages.map((message, index) => (
-            <div
-              key={index}
-              class="flex gap-3 my-4 text-gray-600 text-sm flex-1"
-            >
-              <span class="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
-                {message.role === "user" ? <UserIcon /> : <AiIcon />}
-              </span>
-              <p class="leading-relaxed" ref={messagesEndRef}>
-                <span class="block font-bold text-gray-700">
-                  {message.role === "user" ? "You" : "AI"}{" "}
+    <>
+      <Script
+        src="https://code.responsivevoice.org/responsivevoice.js?key=OziCFq86"
+        strategy="beforeInteractive"
+        onLoad={() =>
+          console.log(
+            `script loaded correctly, responsivevoice  been populated`
+          )
+        }
+      />
+
+      <main className="flex flex-col items-center justify-between w-full h-screen bg-white">
+        <div className="mb-auto overflow-auto w-full">
+          <div className="m-2">
+            {messages.map((message, index) => (
+              <div
+                key={index}
+                className="flex gap-3 my-4 text-gray-600 text-sm flex-1"
+              >
+                <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
+                  {message.role === "user" ? <UserIcon /> : <AiIcon />}
                 </span>
-                {message.content}
-              </p>
-            </div>
-          ))}
+                <p className="leading-relaxed" ref={messagesEndRef}>
+                  <span className="block font-bold text-gray-700">
+                    {message.role === "user" ? "You" : "AI"}{" "}
+                  </span>
+                  {message.content}
+                </p>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-      <div className="mt-auto w-full m-2 p-2">
-        <Input message={message} setMessage={setMessage} onSend={handleSend} />
-      </div>
-    </main>
+        <div className="mt-auto w-full flex justify-around items-center flex-row gap-3 m-2 p-2">
+          <RecordIcon {...attrs} listening={listening ? "true" : "false"} />
+
+          <span className="relative flex  h-8 w-8">
+            <span
+              className={`${
+                listening ? "animate-ping" : ""
+              } absolute inline-flex h-full w-full justify-center items-center rounded-full bg-sky-400 opacity-75`}
+            ></span>
+            <span className="relative inline-flex rounded-full justify-center items-center h-8 w-8 bg-sky-500"></span>
+          </span>
+          <SpeechButton />
+        </div>
+      </main>
+    </>
   );
 }
